1cfa1f9ba8625c95a87fb584458e9aeda36c5165
Ctl-F <broughspencer@gmail.com>
2026-03-06 12:00:14 -0700

    began documentation. added doStreamed to other cases

--- src/dispatch.zig
+++ src/dispatch.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 const util = @import("util");
 
+pub const CasePoolConfig = struct {
+    method: enum { auto, ifChain, nativeSwitch, jumpTable } = .auto,
+};
+
 const Binding = struct {
     definition: type,
     callable: []const u8,
@@ -15,6 +19,42 @@ const Binding = struct {
     }
 };
 
+/// unwrap the cases inside of a tagged-union. This is useful for bulk declaring cases in a tagged union.
+/// The method will go through each enum value in the tag and create a binding for a function with that name
+/// and a provided prefix.
+/// Example:
+/// const Operations = union(enum){
+///    print: i32,
+///    add: Pair,
+///    mul: Pair,
+///
+///    const Pair = struct{ a: i32, b: i32 };
+///
+///    pub fn do_print(ctx: *anyopaque, val: i32) anyerror!void {
+///        _ = ctx;
+///        std.debug.print("Value is: {}\n", .{ val });
+///    }
+///
+///    pub fn do_add(ctx: *anyopaque, val: Pair) anyerror!void {
+///        _ = ctx;
+///        std.debug.print("{} + {} = {}\n", .{ val.a, val.b, val.a + val.b });
+///    }
+///
+///    pub fn do_mul(ctx: *anyopaque, val: Pair) anyerror!void {
+///       _ = ctx;
+///       std.debug.print("{} * {} = {}\n", .{ val.a, val.b, val.a * val.b });
+///    }
+///
+/// };
+/// (...)
+/// fn main() !void {
+///     const dispatch = dispatch.build(Operations, .{ .cases = dispatch.unwrapCases(Operations, "on_") }, .{});
+///
+///     try dispatch.do(&.{}, Pair{ .add = .{ .a = 2, .b = 3 } });
+///
+/// }
+///
+///
 pub fn unwrapCases(bindee: type, comptime prefix: []const u8) []const CasePool(bindee, anyopaque, anyerror).Case {
     const CasePoolType = CasePool(bindee, anyopaque, anyerror);
     const info = @typeInfo(bindee);
@@ -45,6 +85,8 @@ pub fn unwrapCases(bindee: type, comptime prefix: []const u8) []const CasePool(b
     return &casesConcrete;
 }
 
+/// Manually define a case in a set. branchValue is the value that we are matching against and the prong
+/// is a function or binding to execute when the dispatched value is equal to the branch value.
 pub fn case(branchValue: anytype, prong: anytype) CasePool(@TypeOf(branchValue), anyopaque, anyerror).Case {
     return caseWithContext(anyopaque, branchValue, prong);
 }
@@ -70,6 +112,11 @@ pub fn caseWithContext(comptime ContextType: type, branchValue: anytype, prong: 
     //@compileError("Expected type: " ++ @typeName(ExpectedProngType) ++ " or " ++ @typeName(ExpectedProngType.CallableType) ++ " got " ++ @typeName(ProngType));
 }
 
+/// Creates a binding for a function call inside of a type (struct/enum/union/etc)
+/// This is the correct way to call such functions and is the main way that we can allow
+/// duck-typing of tagged-union variant handlers. Bindings are more useful than raw functions
+/// since they can provide error-handler overrides using .withCatch.
+/// TODO: implement makeBinding function to wrap raw functions into a binding allowing for catch semantics.
 pub fn binding(comptime callName: []const u8, comptime callable: type) Binding {
     return Binding{
         .definition = callable,
@@ -78,6 +125,10 @@ pub fn binding(comptime callName: []const u8, comptime callable: type) Binding {
     };
 }
 
+/// This is the main type we define in order to create a dispatch strategy. We define
+/// the BranchType (type we match/switch on), the ContextType (user metadata) and the ErrorType (possible error set returned)
+/// void can be passed in as the context type or error type if those arent needed, however the default is anyopaque and anyerror
+/// for more leinient handling.
 pub fn CasePool(comptime BranchType: type, comptime ContextType: type, comptime ErrorType: type) type {
     return struct {
         const __CasePool__ = @This();
@@ -101,6 +152,7 @@ pub fn CasePool(comptime BranchType: type, comptime ContextType: type, comptime 
             }
         }
 
+        /// a comptime "execution branch" that can be a comptime-known function or a binding.
         pub const Prong = struct {
             const CallableType = fn (ctx: *ContextType, v: BranchType) ErrorType!void;
             const ErrorHandlerType = fn (ctx: *ContextType, v: BranchType, e: ErrorType) ErrorType!void;
@@ -149,6 +201,7 @@ pub fn CasePool(comptime BranchType: type, comptime ContextType: type, comptime 
                 unreachable;
             }
 
+            /// a Runtime execution branch needed for JumpTables.
             const RuntimeProng = struct {
                 callable: *const fn (ctx: *ContextType, v: BranchType) ErrorType!void,
                 catcher: ?*const fn (ctx: *ContextType, v: BranchType, e: ErrorType) ErrorType!void,
@@ -168,6 +221,8 @@ pub fn CasePool(comptime BranchType: type, comptime ContextType: type, comptime 
                 }
             };
         };
+
+        /// A case pairing the branch match value with the execution prong.
         pub const Case = struct {
             value: BranchType,
             prong: __CasePool__.Prong,
@@ -177,36 +232,66 @@ pub fn CasePool(comptime BranchType: type, comptime ContextType: type, comptime 
     };
 }
 
-pub fn build(comptime BranchType: type, comptime casePool: CasePool(BranchType, anyopaque, anyerror)) type {
-    return buildImpl(CasePool(BranchType, anyopaque, anyerror), casePool);
+/// build a CasePool from a branch type and a set of cases
+pub fn build(comptime BranchType: type, comptime casePool: CasePool(BranchType, anyopaque, anyerror), comptime config: CasePoolConfig) type {
+    return buildImpl(CasePool(BranchType, anyopaque, anyerror), casePool, config);
+}
+/// build a CasePool from a branch type and a set of cases
+/// also providing a context type
+pub fn buildWithContextType(comptime BranchType: type, comptime ContextType: type, comptime casePool: CasePool(BranchType, ContextType, anyerror), comptime config: CasePoolConfig) type {
+    return buildImpl(CasePool(BranchType, ContextType, anyerror), casePool, config);
 }
 
-pub fn buildWithContextType(comptime BranchType: type, comptime ContextType: type, comptime casePool: CasePool(BranchType, ContextType, anyerror)) type {
-    return buildImpl(CasePool(BranchType, ContextType, anyerror), casePool);
+/// build a CasePool from a branch type and a set of cases
+/// but also providing the context type and the error type
+pub fn buildWithErrorType(comptime BranchType: type, comptime ContextType: type, comptime ErrorType: type, comptime casePool: CasePool(BranchType, ContextType, ErrorType), comptime config: CasePoolConfig) type {
+    return buildImpl(CasePool(BranchType, ContextType, ErrorType), casePool, config);
 }
 
-pub fn buildWithErrorType(comptime BranchType: type, comptime ContextType: type, comptime ErrorType: type, comptime casePool: CasePool(BranchType, ContextType, ErrorType)) type {
-    return buildImpl(CasePool(BranchType, ContextType, ErrorType), casePool);
-}
+fn buildImpl(comptime CasePoolType: type, comptime casePool: CasePoolType, comptime config: CasePoolConfig) type {
+    // TODO: If an enum set doesn't need optimized-streaming we should fall back to an IfChain so that we aren't generating O(n^2) branches at comptime
+    // we will introduce a flag in config so the user can communicate intent
+    // IfChain -> O(n) branches, faster compilation times and has the potential for similar opimizations in constrained set cases
+    // NativeSwitch -> O(n^2) branches at comptime -> pruned to O(n) runtime branches. Slower comptime, better execution, allows for threaded-dispatch
 
-fn buildImpl(comptime CasePoolType: type, comptime casePool: CasePoolType) type {
-    const ThresholdForIfChain = 1;
+    switch (config.method) {
+        .auto => {},
+        .ifChain => return IfChain(CasePoolType, casePool),
+        .jumpTable => return JumpTable(CasePoolType, casePool),
+        .nativeSwitch => return NativeSwitch(CasePoolType, casePool),
+    }
+    // auto select logic here:
+    // TODO: Improve heuristics to determine which dispatch method to use
+    const ThresholdForIfChain = 5;
 
     if (casePool.cases.len < ThresholdForIfChain) {
         return IfChain(CasePoolType, casePool);
     }
 
-    return NativeSwitch(CasePoolType, casePool);
-
-    //@compileError("Unable to build a dispatch table from provided case pool");
+    switch (@typeInfo(CasePoolType.BranchTy)) {
+        .@"enum", .@"union" => return NativeSwitch(CasePoolType, casePool),
+        .int, .comptime_int => return JumpTable(CasePoolType, casePool), //TODO: add mechanism to coerce ints into enum types and use NativeSwitch for better optimizations
+        else => @compileError("Type not supported for dispatch"), // TODO: Hash-based dispatch for strings/structs/floats/etc.
+    }
 }
 
 pub const StreamedDispatchError = error{NoMatch};
 
+/// IfChain dispatch method. If possible we generate a pattern that zig can optimize (potentially) into a switch statement under the hood
+/// if not we do the naive approach. For small sets this will likely be optimal even without the switch optimization
 fn IfChain(comptime CasePoolType: type, comptime casePool: CasePoolType) type {
     return struct {
+        inline fn isConstrainedSet() bool {
+            return switch (@typeInfo(CasePoolType.BranchTy)) {
+                .@"union", .@"enum" => true,
+                else => false,
+            };
+        }
+
         inline fn casesAreContiguous() bool {
             comptime {
+                if (isConstrainedSet()) return true;
+
                 if (getCasesMinMax()) |minMax| {
                     if (minMax.max - minMax.min == casePool.cases.len and !CasePoolType.hasDuplicates(casePool.cases)) {
                         return true;
@@ -239,14 +324,19 @@ fn IfChain(comptime CasePoolType: type, comptime casePool: CasePoolType) type {
 
         pub inline fn do(ctx: *CasePoolType.ContextTy, value: CasePoolType.BranchTy) (CasePoolType.ErrorTy || StreamedDispatchError)!void {
             if (comptime casesAreContiguous()) {
-                // if cases are min/max able and contiguous e.g. no holes
-                // then we can adopt this pattern which allows us to insert an unreachable
-                // at the end of the inline for and the optimizer can refactor this into a
-                // switch statement.
-                const minMax = getCasesMinMax().?;
+                // the min/max logic isn't needed for a constrained set (enum/union) since you can't generally provide a value outside of an
+                // enum set for comparing.
+                if (comptime !isConstrainedSet()) {
 
-                if (value < minMax.min or value > minMax.max) {
-                    return StreamedDispatchError.NoMatch;
+                    // if cases are min/max able and contiguous e.g. no holes
+                    // then we can adopt this pattern which allows us to insert an unreachable
+                    // at the end of the inline for and the optimizer can refactor this into a
+                    // switch statement.
+                    const minMax = getCasesMinMax().?;
+
+                    if (value < minMax.min or value > minMax.max) {
+                        return StreamedDispatchError.NoMatch;
+                    }
                 }
 
                 inline for (casePool.cases) |caseValue| {
@@ -271,6 +361,20 @@ fn IfChain(comptime CasePoolType: type, comptime casePool: CasePoolType) type {
         pub inline fn doAll(ctx: *CasePoolType.ContextTy, values: []const CasePoolType.BranchTy) (CasePoolType.ErrorTy || StreamedDispatchError)!void {
             for (values) |value| {
                 try @This().do(ctx, value);
+            }
+        }
+
+        //  TODO: Allow bindings for selectors
+        pub inline fn doStreamed(ctx: *CasePoolType.ContextTy, value: CasePoolType.BranchTy, comptime selector: fn (c: *CasePoolType.ContextTy) ?CasePoolType.BranchTy) (CasePoolType.ErrorTy || StreamedDispatchError)!void {
+            var val = value;
+            while (true) {
+                try do(ctx, val);
+
+                if (selector(ctx)) |nextVal| {
+                    val = nextVal;
+                } else {
+                    break;
+                }
             }
         }
     };
@@ -385,6 +489,20 @@ fn JumpTable(comptime CasePoolType: type, comptime casePool: CasePoolType) type 
         pub inline fn doAll(ctx: *CasePoolType.ContextTy, values: []const CasePoolType.BranchTy) (CasePoolType.ErrorTy || StreamedDispatchError)!void {
             for (values) |value| {
                 try @This().do(ctx, value);
+            }
+        }
+
+        pub inline fn doStreamed(ctx: *CasePoolType.ContextTy, value: CasePoolType.BranchTy, comptime selector: fn (c: *CasePoolType.ContextTy) ?CasePoolType.BranchTy) (CasePoolType.ErrorTy || StreamedDispatchError)!void {
+            var val: CasePoolType.BranchTy = value;
+
+            while (true) {
+                try do(ctx, val);
+
+                if (selector(ctx)) |selected| {
+                    val = selected;
+                } else {
+                    break;
+                }
             }
         }
     };
